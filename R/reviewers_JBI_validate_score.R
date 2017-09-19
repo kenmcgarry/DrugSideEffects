cf5f94c6f8bb8b4c5e5ebedfd4387f41b24814da
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2017-09-19 13:48:58 +0100

    Add files via upload

--- R/reviewers_JBI_validate_score.R
+++ R/reviewers_JBI_validate_score.R
@@ -100,23 +100,71 @@ permt.samples[1]
 
 # ============================
 
-R12 <- np.random.rand(50, 100)
-R13 <- np.random.rand(50, 40)
-R23 <- np.random.rand(100, 40)
+# https://www.r-bloggers.com/matrix-factorization/
+require(recommenderlab)
+require(sanealytics)
+data(MovieLense) 
 
-t1 <- fusion.ObjectType('Type 1', 10)
-t2 <- fusion.ObjectType('Type 2', 20)
-t3 <- fusion.ObjectType('Type 3', 30)
-relations <-fusion.Relation(R12, t1, t2)    fusion.Relation(R13, t1, t3) fusion.Relation(R23, t2, t3)
-fusion_graph <- fusion.FusionGraph()
-fusion_graph.add_relations_from(relations)
+unroll_Vecs <- function (params, Y, R, num_users, num_movies, num_features) {
+  # Unrolls vector into X and Theta
+  # Also calculates difference between preduction and actual 
+  
+  endIdx <- num_movies * num_features
+  
+  X     <- matrix(params[1:endIdx], nrow = num_movies, ncol = num_features)
+  Theta <- matrix(params[(endIdx + 1): (endIdx + (num_users * num_features))], 
+                  nrow = num_users, ncol = num_features)
+  
+  Y_dash     <-   (((X %*% t(Theta)) - Y) * R) # Prediction error
+  
+  return(list(X = X, Theta = Theta, Y_dash = Y_dash))
+}
 
-fuser <- fusion.Dfmf()
-fuser.fuse(fusion_graph)
-print(fuser.factor(t1).shape)
+J_cost <-  function(params, Y, R, num_users, num_movies, num_features, lambda, alpha) {
+  # Calculates the cost
+  
+  unrolled <- unroll_Vecs(params, Y, R, num_users, num_movies, num_features)
+  X <- unrolled$X
+  Theta <- unrolled$Theta
+  Y_dash <- unrolled$Y_dash
+  
+  J <-  .5 * sum(   Y_dash ^2)  + lambda/2 * sum(Theta^2) + lambda/2 * sum(X^2)
+  
+  return (J)
+}
+
+grr <- function(params, Y, R, num_users, num_movies, num_features, lambda, alpha) {
+  # Calculates the gradient step
+  # Here lambda is the regularization parameter
+  # Alpha is the step size
+  
+  unrolled <- unroll_Vecs(params, Y, R, num_users, num_movies, num_features)
+  X <- unrolled$X
+  Theta <- unrolled$Theta
+  Y_dash <- unrolled$Y_dash
+  
+  X_grad     <- ((   Y_dash  %*% Theta) + lambda * X     )
+  Theta_grad <- (( t(Y_dash) %*% X)     + lambda * Theta )
+  
+  grad = c(X_grad, Theta_grad)
+  return(grad)
+}
+
+# Now that everything is set up, call optim
+print(
+  res <- optim(par = c(runif(num_users * num_features), runif(num_movies * num_features)), # Random starting parameters
+               fn = J_cost, gr = grr, 
+               Y=Y, R=R, 
+               num_users=num_users, num_movies=num_movies,num_features=num_features, 
+               lambda=lambda, alpha = alpha, 
+               method = "L-BFGS-B", control=list(maxit=maxit, trace=1))
+)
 
 
+scheme <- evaluationScheme(MovieLense, method = "split", train = .9,k = 1, given = 10, goodRating = 4) 
 
+recommenderRegistry$set_entry(method="RSVD", dataType = "realRatingMatrix", fun=REAL_RSVD,
+  description="Recommender based on Low Rank Matrix Factorization (real data).")
 
 
 
